e125ca84d1f553927d792270f73ea1a6897ea696
Alibek <52044412+Alibek15@users.noreply.github.com>
2019-09-25 01:18:11 +0600

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+     public class Bank
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Deposit { get; set; }
+        public int Withdraw { get; set; }
+        public int GetCurrentBalance { get; set; }
+
+    }
+}
